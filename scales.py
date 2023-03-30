274eb219f42bbe54df679f8f057f8c8a0e97553f
binarybrosef <dylan.stadler@gmail.com>
2023-03-30 15:04:03 -0700

    Scales file.
    File from which Fretboard populates scale selection. Scales are encoded in dict format. Include in same directory as fretboard.py.

---
+++ scales.py
@@ -0,0 +1,42 @@
+scaleDict = {'maj_min':    [(0,2,4,5,7,9,11), ['Major/Minor'],
+                            ['Major/Ionian', 'Dorian', 'Phrygian', 'Lydian', 
+                            'Mixolydian','Minor/Aeolian', 'Locrian']],
+
+              'mel_min':    [(0,2,3,5,7,9,11), ['Melodic Minor'],
+                            ['Melodic Minor', 'Dorian b2/Phrygian #6', 'Lydian Augmented',
+                             'Lydian Dominant', 'Mixolydian b6', 'Aeolian b5/Locrian #2',
+                             'Altered/Super Locrian']],
+
+              'harm_min':   [(0,2,3,5,7,8,11), ['Harmonic Minor'],
+                            ['Harmonic Minor/Aeolian #7', 'Locrian #6', 'Ionian #5/Augmented Major',
+                             'Ukranian Dorian', 'Phrygian Dominant', 'Lydian #2', 
+                             'Superlocrian bb7/Altered Diminished']],
+
+              'harm_maj':   [(0,2,4,5,7,8,11), ['Harmonic Major'],
+                            ['Harmonic Major', 'Locrian #2 #6/Dorian b5', 'Altered Dominant #5/Phrygian b4',
+                             'Melodic Minor #4/Lydian b3', 'Mixolydian b2', 'Lydian Augmented #2',
+                              'Locrian bb7']],      
+
+              'half_whole': [(0,1,3,4,6,7,9,10), ['Half-Whole Diminished/Octatonic'],
+                            ['Half-Whole Diminished 1', 'Whole-Half Diminished 1',
+                             'Half-Whole Diminished 2', 'Whole-Half Diminished 2',
+                             'Half-Whole Diminished 3', 'Whole-Half Diminished 3',
+                             'Half-Whole Diminished 4', 'Whole-Half Diminished 4',]],
+
+              'whole_tone': [(0,2,4,6,8,10), ['Whole-tone'], ['Whole-tone 1', 'Whole-tone 2', 'Whole-tone 3',
+                             'Whole-tone 4', 'Whole-tone 5', 'Whole-tone 6']],
+
+              'pentatonic': [(0,3,5,7,10), ['Pentatonic'], ['Pentatonic 1', 'Pentatonic 2',
+                             'Pentatonic 3', 'Pentatonic 4', 'Pentatonic 5']],
+
+              'chromatic':  [(0,1,2,3,4,5,6,7,8,9,10,11), ['Chromatic'], ['Chromatic 1', 'Chromatic 2',
+                             'Chromatic 3', 'Chromatic 4', 'Chromatic 5', 'Chromatic 6', 'Chromatic 7',
+                             'Chromatic 8', 'Chromatic 9', 'Chromatic 10', 'Chromatic 11', 'Chromatic 12']],
+
+              'major6_dim': [(0,2,4,5,7,8,9,11), ['Major 6th Diminished'],
+                            ['Major 6 Dim', 'Dorian Dim', 'Phrygian', 'Lydian', 
+                            'Mixolydian','Minor/Aeolian', 'Locrian']],
+
+              'bebop_dom':  [(0,2,4,5,7,9,10,11), ['Bebop Dominant'], ['Bebop Dominant 1', 'Bebop Dominant 2',
+                             'Bebop Dominant 3', 'Bebop Dominant 4', 'Bebop Dominant 5', 'Bebop Dominant 6',
+                             'Bebop Dominant 7', 'Bebop Dominant 8']]}
